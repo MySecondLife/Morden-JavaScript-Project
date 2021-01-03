30101f89200c61c0c7e0e90bbc7e3cfc8f011fcf
MySecondLife <773882206@qq.com>
2021-01-03 17:07:47 +0800

    add pre-commit hook

--- src/FirstLevel.jsx
+++ src/FirstLevel.jsx
@@ -1,25 +1,30 @@
-import React from 'react';
+import React from "react";
 
-import './FirstLevel.scss';
+import "./FirstLevel.scss";
 
 export default class FirstLevel extends React.Component {
-    constructor() {
-        this.state = {
-            x: 1
-        }
-    }
+  constructor() {
+    this.state = {
+      x: 1,
+    };
+  }
 
-    componentDidMount() {
-        console.log('you did a great job!');
-    }
+  componentDidMount() {
+    console.log("you did a great job!");
+  }
 
-    goodBoy = () => {
-        if (this.state.x === 1) {
-            console.log('bybe le ning');
-        }
+  goodBoy = () => {
+    if (this.state.x === 1) {
+      console.log("bybe le ning");
     }
+  };
 
-    render() {
-        return <div className="first-level test">She is a genuine person and the food is authentic Sichuan cuisine, and the process is legitimate!</div>
-    }
+  render() {
+    return (
+      <div className="first-level test">
+        She is a genuine person and the food is authentic Sichuan cuisine, and
+        the process is legitimate!<span>test prettier</span>
+      </div>
+    );
+  }
 }
