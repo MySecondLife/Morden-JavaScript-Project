90641fc514bdd959a8120fb455499c2c805995de
MySecondLife <773882206@qq.com>
2021-01-13 23:09:24 +0800

    1.add analyze tools for webpack 2.reduce the parse time by adding ext of file, ruling path for moudle, taking advantages of multiple core of cup and using es6 module

--- build/webpack.client.js
+++ build/webpack.client.js
@@ -8,15 +8,21 @@ const env = process.env;
 const isProductionEnv = env.NODE_ENV === "production";
 // require("@babel/polyfill"); // 三种方法设置polyfill 1在.babelrc中进行设置 2webpack入口设置 entry 3直接引入文件至网页文件中
 const HappyPack = require("happypack"); // 打开多进程进行打包构建，提高构建速度；结合js jsx module和插件
+const TerserPlugin = require("terser-webpack-plugin"); // webpack5后可以不安装，默认启用； 项目小的话构建时间可能会增加
 
 const StyleLintPlugin = require("stylelint-webpack-plugin");
 const ESLintPlugin = require("eslint-webpack-plugin");
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin"); // 对项目构建进行时间分析
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin; // 打包文件进行分析；脚手架完成后可删除
+
+const smp = new SpeedMeasurePlugin();
 
 console.log(env.client_host_url);
 console.log(env.version);
 console.log(env.NODE_ENV);
 
-module.exports = {
+module.exports = smp.wrap({
   mode: env.NODE_ENV === "production" ? "production" : "development",
   entry: {
     main1: "./src/index.jsx",
@@ -27,7 +33,10 @@ module.exports = {
     filename: "[name].[contentHash:8].js",
   },
   resolve: {
-    extensions: [".jsx", ".js"],
+    extensions: [".jsx", ".js"], // 模块引用可省略后缀，但不要写不可能出现的情况，会增加解析时间
+    modules: [path.resolve(__dirname, "src"), "node_modules"], // 告诉 webpack 解析模块时应该搜索的目录
+    // alias: {} // 配置常用组件的名称，可以webpack查找快速匹配，写法干净
+    mainFields: ["jsnext:main", "browser", "main"], // 使用版本顺序
   },
   module: {
     rules: [
@@ -87,6 +96,9 @@ module.exports = {
         },
       },
     },
+    minimizer: [
+      new TerserPlugin(), // 默认parallel开启
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(),
@@ -107,6 +119,7 @@ module.exports = {
       id: "babel", // HappyPack实例的ID
       loaders: ["babel-loader?cacheDirectory"], // HappyPack实例使用的loader
     }),
+    new BundleAnalyzerPlugin(),
     // new webpack.optimize.ModuleConcatenationPlugin()  set mode:production情况下，自动执行，合并模块！
   ],
   devServer: {
@@ -114,4 +127,4 @@ module.exports = {
     host: "localhost",
     open: true,
   },
-};
+});
